a850b1eb4b546d0019c544ca3992db93ed3e8b0b
jennifer <cxxyao2@gmail.com>
2025-03-26 20:12:51 -0400

    lexampe

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-const server = import("../dist/ssr-demo2/server/server.mjs");
-
-module.exports = server.app;
-
-// export default import('../dist/ssr-demo2/server/server.mjs').then(module => module.app());
+export default import("../dist/ssr-demo2/server/server.mjs").then((module) =>
+  module.app()
+);
