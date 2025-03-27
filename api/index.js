2db6afe79cf94a7f58ee484eb0148d87450b2578
jennifer <cxxyao2@gmail.com>
2025-03-26 20:01:06 -0400

    correct type

--- api/index.js
+++ api/index.js
@@ -1,2 +1,3 @@
-export default import('../dist/project/server/server.mjs')
-  .then(module => module.app());
+export default import("../dist/ssr-demo2/server/server.mjs").then((module) =>
+  module.app()
+);
