12b593348cd51a798443ee6e2b0e6f9531481e2a
jennifer <cxxyao2@gmail.com>
2025-03-26 20:11:18 -0400

    modify function

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
-const server = import("../dist/project/server/server.mjs");
+const server = import("../dist/ssr-demo2/server/server.mjs");
 
 module.exports = server.app;
+
+// export default import('../dist/ssr-demo2/server/server.mjs').then(module => module.app());
